e4fe2818708db9476c6c2764257487d5311df859
Your Name <huyhuhyhoa@gmail.com>
2023-12-01 17:17:13 +0700

    ruby_on_rails_v2

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
- 
-  get 'static_page/home'
-  get 'static_page/help'
-  get 'static_page/contact'
+  get 'users/new'
+  get '/', to: "static_page#home"
+  get '/home', to: 'static_page#home'
+  get '/help', to: 'static_page#help'
+  get '/contact', to: 'static_page#contact'
+  get '/signup', to: 'users#new'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :users
+
 end
